1731df40b4e8c7adc6c97841fbbcd494a5b01ab3
Sumanth Neerumalla <sumanth42gold@gmail.com>
2016-07-22 23:18:54 -0400

    reorganized

--- js/game.js
+++ js/game.js
@@ -52,3 +52,20 @@ function gameload() {
     var img=document.getElementById("background");
     ctx.drawImage(img,0,0);
 };
+
+function response( req, data )
+{
+    console.log( "response: " + data );
+}
+
+function CommsLoop()
+{
+    setTimeout( CommsLoop, 100 );
+
+    var MyObject = { x:5 };
+
+    QueueOperation(MyObject, response );
+}
+
+
+setTimeout( CommsLoop, 100 );
